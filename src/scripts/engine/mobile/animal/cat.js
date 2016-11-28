f0cb8b4beba5b996ddd4898f06e0d24fd270b690
Deniz Traka <deniz.traka@gmail.com>
2016-11-28 17:46:20 +0300

    chicken and cat .pngs replaced

--- src/scripts/engine/mobile/animal/cat.js
+++ src/scripts/engine/mobile/animal/cat.js
@@ -1,10 +1,14 @@
 'use strict';
 function Cat(game,x,y){    
-    Animal.call(this,game,x,y,"cat");    
-    this.animations.add('left', [12, 13,14], 10, true);
-    this.animations.add('right', [24, 25, 26], 10, true);
-    this.animations.add('up', [36, 37, 38], 10, true);
-    this.animations.add('down', [0, 1, 2], 10, true);
+    Animal.call(this,game,x,y,"cat");  
+    this.animations.add('left', [3, 4,5], 10, true);
+    this.animations.add('right', [6, 7, 8], 10, true);
+    this.animations.add('up', [0, 1, 2], 10, true);
+    this.animations.add('down', [9, 10, 11], 10, true);  
+    // this.animations.add('left', [81, 82, 83], 10, true);
+    // this.animations.add('right', [93, 94, 95], 10, true);
+    // this.animations.add('up', [105, 106, 107], 10, true);
+    // this.animations.add('down', [69, 70, 71], 10, true);
 }
 
 Cat.prototype = Object.create(Animal.prototype);
--- src/scripts/preloader.js
+++ src/scripts/preloader.js
@@ -15,7 +15,7 @@ DGame.Preloader.prototype = {
 		this.load.spritesheet('trees', 'assets/img/spritesheets/EasyTiles_Trees_16x32.png', 16, 32);
 		this.load.spritesheet('player', 'assets/img/spritesheets/spaceman.png', 16, 16);
 		this.load.image('pig', 'assets/img/tiles/agac3.png', 16, 16);
-		this.load.spritesheet('cat', 'assets/img/spritesheets/animals/cats.png', 16, 16);
+		this.load.spritesheet('cat', 'assets/img/spritesheets/animals/cat.png', 16, 16);
 		this.load.spritesheet('chicken', 'assets/img/spritesheets/animals/chicken.png', 16, 16);
 	},
 	create: function () {
